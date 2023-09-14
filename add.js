45eef4d1c3f8a425a7122b3996c3051dbd769141
Rekecki Petra <h163296@stud.u-szeged.hu>
2023-09-14 20:11:06 +0100

    Form validation

---
+++ add.js
@@ -0,0 +1,29 @@
+const img = document.getElementById('image')
+const name = document.getElementById('name')
+const author = document.getElementById('author')
+const date = document.getElementById('date')
+
+
+const addform = document.getElementById('addform')
+const errorElement = document.getElementById('error')
+
+addform.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+    else if (author.value === '' || author.value == null){
+        messages.push('Author is required')
+    }
+    else if (date.value === '' || date.value == null){
+        messages.push('Date is required')
+    }
+    else if (date.value > 2023 || date.value < 1900){
+        messages.push('Wrong date')
+    }
+    if (messages.length > 0){
+    e.preventDefault()
+    errorElement.innerText
+        = messages.join(',')
+    }
+})

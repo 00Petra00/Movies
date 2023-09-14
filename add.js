cb5a35d6f17a849abbcdcfdc993773687a07c9b8
Rekecki Petra <h163296@stud.u-szeged.hu>
2023-09-14 20:11:34 +0100

    Form validation

--- add.js
+++ add.js
@@ -3,7 +3,6 @@ const name = document.getElementById('name')
 const author = document.getElementById('author')
 const date = document.getElementById('date')
 
-
 const addform = document.getElementById('addform')
 const errorElement = document.getElementById('error')
 
@@ -21,6 +20,21 @@ addform.addEventListener('submit', (e) => {
     else if (date.value > 2023 || date.value < 1900){
         messages.push('Wrong date')
     }
+
+    img.addEventListener("change", function() {
+
+    const file = img.files[0];
+
+    if(file) {
+        const fileName = file.name;
+        const fileExtension = fileName.split(".").pop();
+        if (fileExtension.toLowerCase() != "jpg" || fileExtension.toLowerCase() != "jpeg" || fileExtension.toLocaleString() != "png") {
+            messages.push("Only jpg/jpeg and png files are allowed!")
+        }
+    }
+    })
+
+
     if (messages.length > 0){
     e.preventDefault()
     errorElement.innerText
---
+++ list.js
@@ -0,0 +1,15 @@
+let movies = [{name: "Teszt", author: "Hejj", date: 2023},
+              {name: "Proba", author: "Dsfdsf", date: 2000}
+             ]
+let container = document.getElementById("grid")
+
+
+for (let i = 0; i < movies.length; i++){
+    let movie = movies[i]
+    let divPos = document.createElement("div")
+    divPos.className = "pos"
+
+}
+// document.getElementById('name').innerHTML = name
+// document.getElementById('author').innerHTML = author
+// document.getElementById('date').innerHTML = date
